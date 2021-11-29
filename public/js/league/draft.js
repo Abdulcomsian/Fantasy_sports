2cc0e8a5e0368c21c30df3b163ae097d1ece5923
Abdul Basit <basitawan.abdul@gmail.com>
2021-11-29 16:11:39 +0500

    updated code

--- public/js/league/draft.js
+++ public/js/league/draft.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         } else {
             console.log("hello");
             $(".city_board_table").css("margin-top", "0px");
-            $(".city_board_table table").css("margin-top", "170px");
+            $(".city_board_table table").css("margin-top", "145px");
             $(".test2 table").css("margin-top", "86px !important");
 
         }
